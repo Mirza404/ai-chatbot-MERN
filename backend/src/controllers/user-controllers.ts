163765a9f6f480b1cb0ab24e04a8348de028aa0a
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2024-07-04 01:02:30 +0200

    Login and signup controllers completed. Authentication functionality added.

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/tokens-manager.js";
+import { COOKIE_NAME } from "../utils/consts.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -30,6 +32,25 @@ export const userLogin = async (
       return res.status(403).send("Incorrect Password");
     }
 
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error.message);
@@ -49,6 +70,26 @@ export const userSignup = async (
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     user.save();
+
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error.message);
