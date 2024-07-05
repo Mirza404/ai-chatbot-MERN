2d985c4aea8e7da850305e075236d2d49a9120ef
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2024-07-05 17:58:35 +0200

    Changes done to frontend. Clickable logo icon added to the header component.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { AppBar } from "@mui/material";
+import { Toolbar } from "@mui/material";
+import Logo from "./shared/Logo";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{display: "flex"}}>
+        <Logo/>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
---
+++ frontend/src/components/shared/Logo.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+
+const Logo = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        marginRight: "auto",
+        alignItems: "center",
+        gap: "8px",
+      }}
+    >
+      <Link to={"/"}>
+        <img
+          src="4.png"
+          alt="openai"
+          width={"40px"}
+          height={"40px"}
+          className="image-inverted"
+        />
+        <Typography
+          sx={{
+            display: { md: "block", sm: "none", xs: "none" },
+            mr: "auto",
+            fontWeight: " 600",
+            textShadow: "2px 2px 20px #000",
+          }}
+        >
+          <span style={{ fontSize: "20px" }}>MERN</span>-GPT
+        </Typography>
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
