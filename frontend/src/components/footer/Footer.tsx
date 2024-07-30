6b20a46b29c72795b403e213b2ab18020f1740d8
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2024-07-30 15:47:51 +0200

    Credits added to the home page. Update complete

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"; 
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          padding: 20,
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 50,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center" }}>
+          👾 Built with love by {""} <span><Link className="nav-link" to={"https://github.com/Mirza404"}>Mirza404</Link></span>👾
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
