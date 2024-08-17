1a14f987d3caa630b5a5af677625b3965ace715c
Mirza Abdulahovic <mirzabdulahovic@gmail.com>
2024-08-17 19:16:32 +0200

    Further frontend changes made

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,21 +1,38 @@
 import { Link } from "react-router-dom"; 
+// import './index.css'; // Ensure you import the CSS file
+
+// const Footer = () => {
+//   return (
+//     <footer>
+//       <div
+//         style={{
+//           width: "100%",
+//           padding: 20,
+//           minHeight: "20vh",
+//           maxHeight: "30vh",
+//           marginTop: 50,
+//         }}
+//       >
+//         <p style={{ fontSize: "30px", textAlign: "center" }}>
+//           Built with love by <span></span>
+//           <Link className="nav-link" to={"https://github.com/Mirza404"}>Mirza404</Link>
+//         </p>
+//       </div>
+//     </footer>
+//   );
+// };
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
   return (
-    <footer>
-      <div
-        style={{
-          width: "100%",
-          padding: 20,
-          minHeight: "20vh",
-          maxHeight: "30vh",
-          marginTop: 50,
-        }}
-      >
-        <p style={{ fontSize: "30px", textAlign: "center" }}>
-          👾 Built with love by <span><Link className="nav-link" to={"https://github.com/Mirza404"}>Mirza404</Link></span>👾
-        </p>
-      </div>
+    <footer className="footer">
+      <Link className="footer-link" to={"https://github.com/Mirza404"}>
+        <FontAwesomeIcon icon={faGithub} />
+        Mirza404
+      </Link>
     </footer>
   );
 };
